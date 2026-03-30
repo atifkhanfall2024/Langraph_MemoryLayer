155cd9dab8577ba70c2eb3d067ab48b084ac5b08
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2026-03-31 02:22:18 +0500

    neo4j work fine

--- MeM0/mem1.py
+++ MeM0/mem1.py
@@ -1,11 +1,25 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 from mem0 import Memory
 from dotenv import load_dotenv
 import os
+from qdrant_client import QdrantClient
+
 
 load_dotenv()
 
 GROQ_KEY = os.getenv("GROQ_API_KEY")
 HF_TOKEN = os.getenv("HF_TOKEN")  # Hugging Face token for embeddings
+
+client = QdrantClient(host="localhost", port=6333)
+
+client.recreate_collection(
+    collection_name="mem0",
+    vectors_config={
+        "size": 384,
+        "distance": "Cosine"
+    }
+)
 
 config = {
     "version": "v1.1",
@@ -28,9 +42,11 @@ config = {
   "graph_store": {
     "provider": "neo4j",
     "config": {
-        "url": "neo4j://8f00d32f.databases.neo4j.io",
+        "url": "neo4j+ssc://8f00d32f.databases.neo4j.io",
         "username": "neo4j",
-        "password": "SOpvFH8dm-QqHltMFIQpoApy2icz4lFM8l0SDuzYj04"
+        "password": "SOpvFH8dm-QqHltMFIQpoApy2icz4lFM8l0SDuzYj04",
+        "database":"graph_store",
+        "verify_ssl": False 
       
     }
 },
@@ -65,6 +81,7 @@ while True:
 You are an AI assistant.
 
 Use the following user memories to answer the question:
+you pick the answer from memory if user ask and that present in memory 
 
 {memories}
 
